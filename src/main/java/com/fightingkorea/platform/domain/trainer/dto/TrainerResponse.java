0629b423a8eec0c5386a7d3cc729f64f3db6941f
HwangSlater <doorchan23@naver.com>
2025-08-18 03:08:39 +0900

    s

--- src/main/java/com/fightingkorea/platform/domain/trainer/dto/TrainerResponse.java
+++ src/main/java/com/fightingkorea/platform/domain/trainer/dto/TrainerResponse.java
@@ -2,12 +2,16 @@ package com.fightingkorea.platform.domain.trainer.dto;
 
 import com.fightingkorea.platform.domain.user.dto.UserResponse;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class TrainerResponse {
     private Long trainerId;
 
