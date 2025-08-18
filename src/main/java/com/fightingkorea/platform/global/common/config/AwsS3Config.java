b9b2943095f5217116e73bae79d705df35b3ac0a
HwangSlater <doorchan23@naver.com>
2025-08-19 00:34:13 +0900

    properties

--- src/main/java/com/fightingkorea/platform/global/common/config/AwsS3Config.java
+++ src/main/java/com/fightingkorea/platform/global/common/config/AwsS3Config.java
@@ -1,8 +1,11 @@
 package com.fightingkorea.platform.global.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -10,19 +13,27 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class AwsS3Config {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-                .region(Region.AWS_GLOBAL) // 필요 시 Region.of("ap-northeast-2") 로 고정
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of("ap-northeast-2"))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
     @Bean
     public S3Presigner s3Presigner() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Presigner.builder()
-                .region(Region.AWS_GLOBAL) // 위와 동일 region 사용
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of("ap-northeast-2"))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
